699c10fc27c9d267aa7c34ce063d6f6be580f46e
AhaD <abuahad35188@gmail.com>
2023-04-10 23:15:25 +0600

    last card image solve

--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -8,13 +8,14 @@ const Jobs = ({allJob}) => {
      return (
           <div>
                <div>
-               <div className='border border-gray-400    rounded-xl m-auto mb-5 mt-5 p-10'>
+               <div className='border border-gray-400 md:flex justify-evenly   rounded-xl m-auto mb-5 mt-5 p-10'>
                       <img className='w-96' src={image} alt="" />
+               <div>
                <div className="p-5">
                <h2 className='text-2xl font-bold mb-2'>{jobTitle}</h2>
                <h3 className='text-xl font-medium mb-2'>{companyName}</h3>
                <button className='p-2 bg-slate-50'>{jobStatus}</button>
-                <div className='md:flex justify-between items-center mt-2'>
+                <div className=' ml-5 mt-2'>
                     <p className='mb-2'>Address:{address}</p>
                     <p>Salary: {salary} k</p>
                 </div>
@@ -22,7 +23,7 @@ const Jobs = ({allJob}) => {
                     <button onClick={()=>navigate(`/jobs/${id}`)} title='Full Details' className='mt-8 text-center bg-purple-500 p-3'>View Details</button>
                
                </div>
-
+               </div>
           </div>
                </div>
           </div>
